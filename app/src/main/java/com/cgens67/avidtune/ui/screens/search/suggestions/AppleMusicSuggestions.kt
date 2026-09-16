8f78576ea337ffa07f2bda25b4204846287cc52b
ombarainyt <ombarainyt@gmail.com>
2026-09-16 19:40:01 +0800

    Update AppleMusicSuggestions.kt

--- app/src/main/java/com/cgens67/avidtune/ui/screens/search/suggestions/AppleMusicSuggestions.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/screens/search/suggestions/AppleMusicSuggestions.kt
@@ -1,3 +1,9 @@
+@file:OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3ExpressiveApi::class,
+    ExperimentalFoundationApi::class
+)
+
 package com.cgens67.gluetune.ui.screens.search.suggestions
 
 import android.widget.Toast
@@ -24,33 +30,14 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -108,7 +95,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
@@ -131,7 +117,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.cgens67.gluetune.LocalPlayerAwareWindowInsets
 import com.cgens67.gluetune.LocalPlayerConnection
 import com.cgens67.gluetune.R
@@ -371,7 +356,6 @@ class SuggestionsViewModel @Inject constructor() : ViewModel() {
 
 // --- Main Composable Screen ---
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppleMusicTrendingScreen(
     navController: NavController,
@@ -619,7 +603,7 @@ fun AppleMusicTrendingScreen(
                         scaleY = scaleFraction()
                     }
             ) {
-                PullToRefreshDefaults.LoadingIndicator(
+                PullToRefreshDefaults.Indicator(
                     state = pullToRefreshState,
                     isRefreshing = isLoading
                 )
@@ -1273,7 +1257,6 @@ private fun AppleMusicArtistsRow(
 
 // --- Region Selector BottomSheet ---
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun RegionSelectorBottomSheet(
     currentRegion: String,
