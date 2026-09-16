97caef4ff9f5678e8fa5d0d909c311bb7d9032ae
ombarainyt <ombarainyt@gmail.com>
2026-09-16 18:57:16 +0800

    Update Player.kt

--- app/src/main/java/com/cgens67/avidtune/ui/player/Player.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/player/Player.kt
@@ -42,6 +42,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
