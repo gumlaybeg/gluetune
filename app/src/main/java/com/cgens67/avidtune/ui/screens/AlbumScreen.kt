1677296b76cff84e8e8ebb6e466f90a0576c28c2
gumlaybeg <gumlaybeg15@protonmail.com>
2026-09-18 06:47:45 +0800

    Add shadow effect to AlbumScreen

--- app/src/main/java/com/cgens67/avidtune/ui/screens/AlbumScreen.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/screens/AlbumScreen.kt
@@ -96,6 +96,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
