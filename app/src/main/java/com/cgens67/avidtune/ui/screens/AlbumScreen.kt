93cb27ca184446b7d5dc6b156aec04efb149308d
gumlaybeg <gumlaybeg15@protonmail.com>
2026-09-18 20:17:44 +0800

    Add import for systemBars in AlbumScreen.kt

--- app/src/main/java/com/cgens67/avidtune/ui/screens/AlbumScreen.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/screens/AlbumScreen.kt
@@ -54,6 +54,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
