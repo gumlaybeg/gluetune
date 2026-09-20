d6ff4a0e23df711721d647d3d435e0e1ab49c9ff
gumlaybeg <gumlaybeg15@protonmail.com>
2026-09-20 10:50:13 +0800

    Add Offset import to MainActivity.kt

--- app/src/main/java/com/cgens67/avidtune/MainActivity.kt
+++ app/src/main/java/com/cgens67/avidtune/MainActivity.kt
@@ -112,6 +112,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
