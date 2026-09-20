92c803befd9865bb97ca3931fd61fdc777aaee6f
gumlaybeg <gumlaybeg15@protonmail.com>
2026-09-20 19:08:37 +0800

    Refactor theme imports and update color extraction

--- app/src/main/java/com/cgens67/avidtune/ui/theme/Theme.kt
+++ app/src/main/java/com/cgens67/avidtune/ui/theme/Theme.kt
@@ -29,9 +29,9 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.palette.graphics.Palette
 import com.cgens67.gluetune.constants.AppFont
+import com.google.material.color.dynamiccolor.DynamicScheme
 import com.google.material.color.dynamiccolor.MaterialDynamicColors
 import com.google.material.color.hct.Hct
-import com.google.material.color.scheme.DynamicScheme
 import com.google.material.color.scheme.SchemeTonalSpot
 import com.google.material.color.score.Score
 
